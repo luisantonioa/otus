517e84ca0dae4f20e999fae8129420e2af9f6128
Luis Arias <luisantonioa@mgail.com>
2013-01-19 20:37:31 +0000

    Inital rails code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Otus::Application.config.secret_token = '74b540d4ca46dd9bbcfa34f18a565813ac1143a11ed37ff48c5bcf00780f6fc771245d334cdaa3d6f85036592cebd61f4ed4d1d52638e0c29cc82aac4fb77b47'
